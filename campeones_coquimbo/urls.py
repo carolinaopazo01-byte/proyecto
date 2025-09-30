f6600222caf56e7210b8d9153e99a5afa14d2afb
cataordeness <catalina.ordenes@alumnos.ucn.cl>
2025-09-29 22:16:39 -0300

    Prueba

--- campeones_coquimbo/urls.py
+++ campeones_coquimbo/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from applications.core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('applications.atleta.urls')),         # home y asistencia
-    path('core/', include('applications.core.urls')),      # sedes/deportes
-    path('evals/', include('applications.evaluaciones.urls')),  # planificaciones/evaluaciones
-    path('cuentas/', include('applications.usuarios.urls')),    # login/logout
+    path('', include('applications.core.urls')),
+    path('asistencia/', core_views.asistencia, name='asistencia'),                    # sin curso seleccionado
+    path('asistencia/<int:curso_id>/', core_views.asistencia, name='asistencia_curso'),  # con curso
 ]
