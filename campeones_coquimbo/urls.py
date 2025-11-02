f36990d5c011cc4c8dd919c0decbbc80b918af5b
Constanz03 <153224553+Constanz03@users.noreply.github.com>
2025-11-02 19:57:46 -0300

    commit 123567

--- campeones_coquimbo/urls.py
+++ campeones_coquimbo/urls.py
@@ -1,26 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-from django.conf import settings
+from django.conf import settings              # ← IMPORTANTE
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("core/", include(("applications.core.urls", "core"), namespace="core")),
-    path("atleta/", include(("applications.atleta.urls", "atleta"), namespace="atleta")),
-    path("usuarios/", include(("applications.usuarios.urls", "usuarios"), namespace="usuarios")),
-    path("evaluaciones/", include(("applications.evaluaciones.urls", "evaluaciones"), namespace="evaluaciones")),
-    path("pmul/", include(("applications.pmul.urls", "pmul"), namespace="pmul")),
-    path("apoderado/", include(("applications.apoderado.urls", "apoderado"), namespace="apoderado")),
-    path("profesor/", include(("applications.profesor.urls", "profesor"), namespace="profesor")),
-
-    # raíz -> core:home
-    path("", RedirectView.as_view(pattern_name="core:home", permanent=False)),
-
-    path("cuenta/cambiar-clave/",
-        RedirectView.as_view(pattern_name="usuarios:cambiar_password", permanent=False),
-        name="cambiar_password",
-         ),
+    path("", include("applications.core.urls", namespace="core")),
+    path("atleta/", include("applications.atleta.urls", namespace="atleta")),
+    path("usuarios/", include("applications.usuarios.urls", namespace="usuarios")),
+    path("evaluaciones/", include("applications.evaluaciones.urls", namespace="evaluaciones")),
+    path("pmul/", include("applications.pmul.urls", namespace="pmul")),
+    path("apoderado/", include("applications.apoderado.urls", namespace="apoderado")),
+    path("profesor/", include("applications.profesor.urls", namespace="profesor")),
 ]
 
 if settings.DEBUG:
